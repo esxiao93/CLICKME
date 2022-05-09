51cd3100603b13bff1847fc3f4bf20a74557289f
Eric Xiao <es.xiao93@gmail.com>
2022-05-09 16:37:47 -0400

    added backend db.json server

--- src/Components/ScoreboardForm.js
+++ src/Components/ScoreboardForm.js
@@ -3,21 +3,26 @@ import React, {useState} from 'react'
 function ScoreboardForm({click, onScoreSubmit}) {
 
   const [name, setName] = useState("")
+  const [score, setScore] = useState({click})
 
   function handleName(e) {
     setName(e.target.value)
+  }
+  function handleScore(e) {
+    setScore(e.target.value)
   }
 
   function handleSubmit(e) {
     e.preventDefault()
 
-    fetch('http://localhost:3000', {
+    fetch('http://localhost:8000/scores', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        name: name
+        name: name,
+        score: score
       }),
     })
     .then(response => response.json())
@@ -36,9 +41,9 @@ function ScoreboardForm({click, onScoreSubmit}) {
           />
           <input 
           type="text" 
-          name="score" 
-          placeholder="SCORE" 
+          name="score"
           value={click}
+          onChange={handleScore}
           />
         <button className="ui button" type="submit">
           SUBMIT
